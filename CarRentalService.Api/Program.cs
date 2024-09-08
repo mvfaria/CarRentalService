f748ebdcc6c6dd53bdd9508c874b43dea1d407e3
Marcus Faria <marcusvinicius.faria@gmail.com>
2024-09-08 20:32:53 +0800

    add nswag openapi swagger

--- CarRentalService.Api/Program.cs
+++ CarRentalService.Api/Program.cs
@@ -7,10 +7,32 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddMediatR(cfg =>
     cfg.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
+
 builder.Services.AddSingleton<IReservationRepository, ReservationRepository>();
 builder.Services.AddSingleton<IVehicleTypeRepository, VehicleTypeRepository>();
-builder.Services.AddTransient<IAvailabilityService, AvailabilityService>();
+builder.Services.AddScoped<IAvailabilityService, AvailabilityService>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument(config =>
+{
+    config.DocumentName = "CarRentalService";
+    config.Title = "CarRentalService v1";
+    config.Version = "v1";
+});
 
 var app = builder.Build();
 app.RegisterCarRentalEndpoints();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseOpenApi();
+    app.UseSwaggerUi(config =>
+    {
+        config.DocumentTitle = "CarRentalService";
+        config.Path = "/swagger";
+        config.DocumentPath = "/swagger/{documentName}/swagger.json";
+        config.DocExpansion = "list";
+    });
+}
+
 app.Run();
